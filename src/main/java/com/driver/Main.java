4def20eff0195118666b56a1e35574a34366e107
aman <amanv4775@gmail.com>
2023-02-08 15:21:08 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String args[]){
+        RWOnly obj=new RWOnly();
+        //obj.x=1;
+        //System.out.println(obj.x);
+        obj.setter(5);
+        obj.getter();
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
+    private int x;
+    public void setter(int n){
+        x=n;
+    }
+    public int getter(){
+        return x;
+    }
     
 }
